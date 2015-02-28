3af64ab17c960e66ef8fbc95ab38851f25e5827b
Li Hao <haox.li@intel.com>
2015-02-28 19:42:14 +0800

    Update WebSpeech
    - Update Web Speech following chrome demo
    - Merge android and tizen tests together
    
    Impacted tests(approved): new 0, update 1, delete 0
    Unit test platform: [Android][Crosswalk 13.41.309.0]
    Unit test result summary: pass 1, fail 0, block 0

--- samples/WebSpeech/js/main.js
+++ samples/WebSpeech/js/main.js
@@ -26,90 +26,125 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 Authors:
         Shentu, Jiazhen <jiazhenx.shentu@intel.com>
+        Li, Hao <haox.li@intel.com>
 
 */
 
-var testFlag = {
-    green: false,
-    red: false,
-    blue: false
-};
-var showId;
-
-function status() {
-    if (testFlag.green && testFlag.red && testFlag.blue) {
-        EnablePassButton();
-    }
-}
-
-function show() {
-    $("#infobox").text("Could not connect to speech server. \n" + $("#infobox").text());
-    $("#start").show();
-    $("#stop").hide();
-    $("#abort").attr('disabled', true);
-    clearTimeout(showId);
-}
-
-function start() {
-    testFlag.green = true;
-    $("#start").hide();
-    $("#stop").show();
-    $("#abort").attr('disabled', false);
-    clearTimeout(showId);
-    showId = setTimeout("show()", 10000);
-    window.speechReco = new webkitSpeechRecognition();
-    speechReco.continuous = true;
-    speechReco.interimResults = true;
-    speechReco.onstart = function (evt) {
-        $("#infobox").text("Connecting to speech server. \n" + $("#infobox").text());
-    };
-    speechReco.onresult = function (evt) {
-        for (var i = evt.resultIndex; i < evt.results.length; ++i) {
-            if (evt.results[i].isFinal) {
-                final_transcript += evt.results[i][0].transcript;
-            } else {
-                interim_transcript += evt.results[i][0].transcript;
-            }
-        }
-        $("#infobox").text("SpeechRecognition final - " + final_transcript + "\n" +
-            "SpeechRecognition interim - " + interim_transcript + "\n" + $("#infobox").text());
-    };
-    speechReco.onerror = function(err) {
-        $("#infobox").text("Could not connect to speech server. \nSpeechRecognition error - " + err.error + "\n" + $("#infobox").text());
-        $("#start").show();
-        $("#stop").hide();
-        $("#abort").attr('disabled', true);
-        speechReco.stop();
-        $("#infobox").text("SpeechRecognition stop.\n" + $("#infobox").text());
-    };
-    speechReco.onend = function() {
-        $("#infobox").text("SpeechRecognition end.\n" + $("#infobox").text());
-    };
-    speechReco.start();
-    $("#infobox").text("SpeechRecognition start.");
-    clearTimeout(showId);
-    status();
-}
-
-function stop() {
-    testFlag.red = true;
-    $("#start").show();
-    $("#stop").hide();
-    $("#abort").attr('disabled', true);
-    speechReco.stop();
-    $("#infobox").text("SpeechRecognition stop.\n" + $("#infobox").text());
-    status();
-}
-
-function abort() {
-    testFlag.blue = true;
-    speechReco.abort();
-    $("#infobox").text("SpeechRecognition abort.\n" + $("#infobox").text());
-    status();
-}
+var final_transcript = '';
+var recognizing = false;
+var ignore_onend;
+var start_timestamp;
+var recognition;
 
 $(document).ready(function(){
     DisablePassButton();
-    $("#stop").hide();
-    $("#abort").attr('disabled', true);
+    if (!('webkitSpeechRecognition' in window) && !("tizen" in window)) {
+        upgrade();
+    } else {
+        recognition = "tizen" in window ? new tizen.SpeechRecognition() : new webkitSpeechRecognition();
+        recognition.continuous = true;
+        recognition.interimResults = true;
+
+        recognition.onstart = function() {
+            recognizing = true;
+            showInfo('Speak now.');
+            start_button.className = 'mic-speaking';
+            EnablePassButton();
+        };
+
+        recognition.onerror = function(event) {
+            if (event.error == 'no-speech') {
+                showInfo('No speech was detected. You may need to adjust your microphone settings.');
+                ignore_onend = true;
+            }
+            if (event.error == 'audio-capture') {
+                showInfo('No microphone was found. Ensure that a microphone is installed and that microphone settings are configured correctly.');
+                ignore_onend = true;
+            }
+            if (event.error == 'not-allowed') {
+                if (event.timeStamp - start_timestamp < 100) {
+                    showInfo('Permission to use microphone is blocked.');
+                } else {
+                    showInfo('Permission to use microphone was denied.');
+                }
+                ignore_onend = true;
+            }
+            start_button.className = 'mic';
+        };
+
+        recognition.onend = function() {
+            recognizing = false;
+            if (ignore_onend) {
+                return;
+            }
+            start_button.className = 'mic';
+            if (!final_transcript) {
+                showInfo('Click on the microphone icon and begin speaking for as long as you like.');
+                return;
+            }
+            showInfo('');
+            if (window.getSelection) {
+                window.getSelection().removeAllRanges();
+                var range = document.createRange();
+                range.selectNode(document.getElementById('final_span'));
+                window.getSelection().addRange(range);
+            }
+        };
+
+        recognition.onresult = function(event) {
+            var interim_transcript = '';
+            if (typeof(event.results) == 'undefined') {
+                recognition.onend = null;
+                recognition.stop();
+                upgrade();
+                return;
+            }
+            for (var i = event.resultIndex; i < event.results.length; ++i) {
+                if (event.results[i].isFinal) {
+                  final_transcript += event.results[i][0].transcript;
+                } else {
+                  interim_transcript += event.results[i][0].transcript;
+                }
+            }
+            final_transcript = capitalize(final_transcript);
+            final_span.innerHTML = linebreak(final_transcript);
+            interim_span.innerHTML = linebreak(interim_transcript);
+        };
+    }
 });
+
+function startButton(event) {
+    if (recognizing) {
+            recognition.stop();
+            return;
+    }
+    final_transcript = '';
+    recognition.lang = "en-US";
+    recognition.start();
+    ignore_onend = false;
+    final_span.innerHTML = '';
+    interim_span.innerHTML = '';
+    start_button.className = 'mic-slash';
+    showInfo('Click the "Allow" button above to enable your microphone.');
+    start_timestamp = event.timeStamp;
+}
+
+function upgrade() {
+    start_button.className = 'mic';
+    showInfo('Web Speech API is not supported.');
+}
+
+function showInfo(s) {
+    info.innerHTML = s;   
+}
+
+var two_line = /\n\n/g;
+var one_line = /\n/g;
+function linebreak(s) {
+    return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
+}
+
+var first_char = /\S/;
+function capitalize(s) {
+    return s.replace(first_char, function(m) { return m.toUpperCase(); });
+}
