2d7237d654c86af9b0950030a4b2dc3eca1c082a
zhuyongyong <yongyongx.zhu@intel.com>
2015-06-05 15:06:05 +0800

    [demo-express] Change embedding pkg name to org.xwalk.core.demoexpress
    - Failure analysis: XWALK-2012
    
    Impacted tests(designed|approved): new 0, update 6, delete 0
    Unit test platform: [Android]
    Unit test result summary: pass 5, fail 1, block 0
    
    BUG=https://crosswalk-project.org/jira/browse/XWALK-4345

--- samples-embedding/src/org/xwalk/core/demoexpress/ExtensionEcho.java
+++ samples-embedding/src/org/xwalk/core/demoexpress/ExtensionEcho.java
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.xwalk.embedding.api.demo;
+package org.xwalk.core.demoexpress;
 
 import org.xwalk.core.XWalkExtension;
 
--- samples-embedding/src/org/xwalk/core/demoexpress/XWalkEmbeddedAPISample.java
+++ samples-embedding/src/org/xwalk/core/demoexpress/XWalkEmbeddedAPISample.java
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.xwalk.embedding.api.demo;
+package org.xwalk.core.demoexpress;
 
 import java.util.ArrayList;
 import java.util.List;
